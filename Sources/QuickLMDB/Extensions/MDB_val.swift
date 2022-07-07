8e53df4574c9998b3c1382510fcda32c203bfea1
Tanner Silva <t@tannersilva.com>
2022-07-07 00:42:29 -0500

    more extensions for MDB_val

--- Sources/QuickLMDB/Extensions/MDB_val.swift
+++ Sources/QuickLMDB/Extensions/MDB_val.swift
@@ -1,8 +1,29 @@
-//
-//  File.swift
-//  
-//
-//  Created by Tanner Silva on 7/7/22.
-//
-
+import CLMDB
 import Foundation
+
+extension MDB_val:Hashable {
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(bytes:UnsafeRawBufferPointer(start:self.mv_data, count:self.mv_size))
+	}
+	
+	public static func == (lhs: MDB_val, rhs: MDB_val) -> Bool {
+		if (lhs.mv_size == rhs.mv_size) {
+			return memcmp(lhs.mv_data, rhs.mv_data, lhs.mv_size) == 0
+		} else {
+			return false
+		}
+	}
+}
+
+extension MDB_val:ContiguousBytes {
+	public func withUnsafeBytes<R>(_ body: (UnsafeRawBufferPointer) throws -> R) rethrows -> R {
+		return try body(UnsafeRawBufferPointer(start:self.mv_data, count:self.mv_size))
+	}
+}
+
+extension MDB_val:MDB_encodable {
+	public func asMDB_val<R>(_ valFunc: (inout MDB_val) throws -> R) rethrows -> R {
+		var copyVal = self
+		return try valFunc(&copyVal)
+	}
+}
