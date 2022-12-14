bfd1c28aebf456b2ca92ec089aed67ef014eca42
Tanner Silva <t@tannersilva.com>
2022-12-14 10:17:36 -0600

    removed explicit MacOS requirement

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "QuickLMDB",
-	platforms: [
-		.macOS(.v11)
-	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
