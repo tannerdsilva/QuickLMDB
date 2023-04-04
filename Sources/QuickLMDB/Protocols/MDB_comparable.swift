927e66d6e5312c5e238bd7f722d7078ff82e3311
Tanner Silva <t@tannersilva.com>
2023-04-04 11:00:34 -0500

    fixing typo

--- Sources/QuickLMDB/Protocols/MDB_comparable.swift
+++ Sources/QuickLMDB/Protocols/MDB_comparable.swift
@@ -1,5 +1,5 @@
 
 public protocol MDB_comparable {
-	typealias MDB_conpare_function = @convention(c) (UnsafePointer<MDB_val>?, UnsafePointer<MDB_val>?) -> Int32 
-	static var mdbCompareFunction: MDB_conpare_function { get }
+	typealias MDB_compare_function = @convention(c) (UnsafePointer<MDB_val>?, UnsafePointer<MDB_val>?) -> Int32 
+	static var mdbCompareFunction: MDB_compare_function { get }
 }
