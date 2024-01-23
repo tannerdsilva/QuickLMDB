2cc0c7d69c2f112bec1cf88e23162baf72bc7db1
tannerdsilva <t@tannersilva.com>
2024-01-23 09:14:23 -0600

    struggling to active a macro target

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.5
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "QuickLMDB",
+	platforms: [
+		.macOS(.v10_15)
+	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,9 +15,10 @@ let package = Package(
             targets: ["QuickLMDB"]),
     ],
     dependencies: [
-		.package(url:"https://github.com/tannerdsilva/CLMDB.git", .exact("0.9.30")),
-		.package(url:"https://github.com/tannerdsilva/rawdog.git", from:"5.3.0"),
-		.package(url:"https://github.com/apple/swift-system.git", .upToNextMajor(from:"1.0.0"))
+		.package(url:"https://github.com/tannerdsilva/CLMDB.git", from:"0.9.29"),
+		.package(url:"https://github.com/tannerdsilva/rawdog.git", from:"6.0.0"),
+		.package(url:"https://github.com/apple/swift-system.git", from:"1.0.0"),
+		.package(url:"https://github.com/apple/swift-syntax.git", from:"509.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,6 +30,7 @@ let package = Package(
 				.product(name:"SystemPackage", package:"swift-system"),
 				.product(name:"RAW", package:"rawdog")
 			]),
+		.macro(name:"QuickLMDBMacros", dependencies:[]),
         .testTarget(
             name: "QuickLMDBTests",
             dependencies: ["QuickLMDB"]),
--- Sources/QuickLMDB/Extensions/MDB_val.swift
+++ Sources/QuickLMDB/Extensions/MDB_val.swift
@@ -1,11 +1,22 @@
 import struct CLMDB.MDB_val
 import RAW
 
-// MARK: Static functions
 extension MDB_val {
-	/// Returns an MDB_val that represents a null value.
-	/// - Returned value is a zero-length value with a nil pointer.
-	public static func nullValue() -> MDB_val {
+
+	/// returns a new MDB_val with a null data pointer and a length specified as the encoded count of the given encodable type.
+	internal static func reserved<E>(forRAW_encodable encodable:inout E) -> E {
+		var newEncodable = MDB_val(mv_size:0, mv_data:nil)
+		encodable.RAW_encode(count:&newEncodable.mv_size)
+		return newEncodable
+	}
+
+	/// returns a new MDB_val with a null data pointer and specified length.
+	internal static func reserved(capacity:size_t) -> MDB_val {
+		return MDB_val(mv_size:capacity, mv_data:nil)
+	}
+
+	/// returns an MDB_val with a null data pointer and a length of zero.
+	internal static func nullValue() -> MDB_val {
 		return MDB_val(mv_size:0, mv_data:nil)
 	}
 
