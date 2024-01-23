eb4b89f35c28f1e78c3b16e29e91fb92a0116af6
tannerdsilva <t@tannersilva.com>
2024-01-22 23:01:48 -0600

    now referencing rawdog 5.3.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
 		.package(url:"https://github.com/tannerdsilva/CLMDB.git", .exact("0.9.30")),
-		.package(url:"https://github.com/tannerdsilva/rawdog.git", branch:"v5"),
+		.package(url:"https://github.com/tannerdsilva/rawdog.git", from:"5.3.0"),
 		.package(url:"https://github.com/apple/swift-system.git", .upToNextMajor(from:"1.0.0"))
     ],
     targets: [
