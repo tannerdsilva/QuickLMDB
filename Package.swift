089f0ec443448374d8cbf65f82ddce0c728f980d
tannerdsilva <t@tannersilva.com>
2025-09-27 16:46:55 -0500

    rawdog 20

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies:[
 		.package(url:"https://github.com/tannerdsilva/CLMDB.git", "0.9.26"..<"0.9.31"),
-		.package(url:"https://github.com/tannerdsilva/rawdog.git", "16.0.0"..<"19.0.0"),
+		.package(url:"https://github.com/tannerdsilva/rawdog.git", "20.0.0"..<"21.0.0"),
 		.package(url:"https://github.com/apple/swift-system.git", "1.0.0"..<"2.0.0"),
 		.package(url:"https://github.com/apple/swift-syntax.git", "602.0.0"..<"603.0.0"),
 		.package(url:"https://github.com/apple/swift-log.git", "1.0.0"..<"2.0.0")
