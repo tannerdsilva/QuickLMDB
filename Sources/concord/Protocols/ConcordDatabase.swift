0cb157643f28713f6bc61783ffc663f04a0909c9
tannerdsilva <t@tannersilva.com>
2026-02-08 17:13:30 -0600

    wip

--- Sources/concord/Protocols/ConcordDatabase.swift
+++ Sources/concord/Protocols/ConcordDatabase.swift
@@ -4,11 +4,22 @@ import RAW
 /// a protocol used to express databases that can be automatically synced by way of a shared type of key.
 public protocol ConcordDatabase:MDB_db_strict, Sendable where Self.MDB_db_key_type:IndexVector {}
 
-
 extension MDB_cursor {
-	internal func fingerprintShift<H>(begin:consuming MDB_val, bucketSize:Int, hasher:H.Type) throws -> H.RAW_hasher_outputtype where H:RAW_hasher {
+	internal func fingerprint<H>(begin:consuming MDB_val, bucketSize:Int, hasher:H.Type) throws -> H.RAW_hasher_outputtype where H:RAW_hasher {
 		var hasher = try H()
 		for (key, _) in view(begin:begin, steps:bucketSize) {
+			try hasher.update(key.mv_data, count:key.mv_size)
+		}
+		var returnData = H.RAW_hasher_outputtype(RAW_staticbuff:H.RAW_hasher_outputtype.RAW_staticbuff_zeroed())
+		try returnData.RAW_access_staticbuff_mutating { outputData in
+			try hasher.finish(into:outputData)
+		}
+		return returnData
+	}
+	
+	internal func fingerprint<H>(begin:consuming MDB_val, end:consuming MDB_val, hasher:H.Type) throws -> H.RAW_hasher_outputtype where H:RAW_hasher {
+		var hasher = try H()
+		for (key, _) in view(begin:begin, end:end) {
 			try hasher.update(key.mv_data, count:key.mv_size)
 		}
 		var returnData = H.RAW_hasher_outputtype(RAW_staticbuff:H.RAW_hasher_outputtype.RAW_staticbuff_zeroed())
